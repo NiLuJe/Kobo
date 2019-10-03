6e2ae04b4deba0e0823bd591b6424f157ded6784
NiLuJe <ninuje@gmail.com>
2019-10-03 20:02:44 +0200

    Check that the daemon started successfully

--- MiniClock/usr/local/MiniClock/miniclock.sh
+++ MiniClock/usr/local/MiniClock/miniclock.sh
@@ -327,7 +327,13 @@ fbink_check() {
                     --truetype "$truetype",size="$cfg_truetype_size",px="$cfg_truetype_px",top="$cfg_truetype_y",bottom=0,left="$cfg_truetype_x",right=0,format \
                     -C "$cfg_truetype_fg" -B "$cfg_truetype_bg" \
                     $nightmode)"
-        fbink_with_truetype=1
+        if [ $? -eq 0 ]
+        then
+            fbink_with_truetype=1
+        else
+            fbink_pid=''
+            fbink_with_truetype=-1
+        fi
 
         debug_log && do_debug_log "-- launched truetype FBInk daemon --"
     else
@@ -346,7 +352,13 @@ fbink_check() {
                     -F "$cfg_font" -S "$cfg_size" \
                     -C "$cfg_fg_color" -B "$cfg_bg_color" \
                     $nightmode)"
-        fbink_with_truetype=0
+        if [ $? -eq 0 ]
+        then
+            fbink_with_truetype=0
+        else
+            fbink_pid=''
+            fbink_with_truetype=-1
+        fi
 
         debug_log && do_debug_log "-- launched bitmap FBInk daemon --"
     fi
